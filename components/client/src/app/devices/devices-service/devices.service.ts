f5eaea5c9ec55c54438ce813c396357c9551def4
Kyle <kyleperalestudio@gmail.com>
2024-10-03 03:22:43 +0800

    Fix scss routes and clean up devices service

--- components/client/src/app/devices/devices-service/devices.service.ts
+++ components/client/src/app/devices/devices-service/devices.service.ts
@@ -4,21 +4,23 @@ import { Observable } from 'rxjs'
 import { env } from '../../../environment'
 import { IDevice, IUpdateControl, IUpdateResponse } from './device.model'
 
+const DEVICES_ROUTE = '/api/v1/devices/'
+
 @Injectable({ providedIn: 'root' })
 export class DevicesService {    
   constructor(
-      private http: HttpClient
+    private http: HttpClient
   ) { }
 
   getDevices(): Observable<IDevice[]> {
-    return this.http.get<IDevice[]>(`${env.baseUrl}/api/v1/devices/`)
+    return this.http.get<IDevice[]>(`${env.baseUrl}${DEVICES_ROUTE}`)
   }
 
   getDeviceById(id: string): Observable<IDevice> {
-    return this.http.get<IDevice>(`${env.baseUrl}/api/v1/devices/${id}`)
+    return this.http.get<IDevice>(`${env.baseUrl}${DEVICES_ROUTE}${id}`)
   }
 
   updateDevice(id: string, controls: IUpdateControl[]): Observable<IUpdateResponse> {
-    return this.http.post<IUpdateResponse>(`${env.baseUrl}/api/v1/devices/${id}`, { controls })
+    return this.http.post<IUpdateResponse>(`${env.baseUrl}${DEVICES_ROUTE}${id}`, { controls })
   }
 }
